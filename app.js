e231647a40df422213caaa31e1435e5af2a7a6a0
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-10-18 14:13:02 +1000

    feat: add task 4.2 Blog list, step2 solution

---
+++ app.js
@@ -0,0 +1,39 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const blogsRouter = require('./controllers/blogs.js');
+
+const config = require('./utils/config.js');
+const logger = require('./utils/logger.js');
+const middleware = require('./utils/middleware.js');
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('build'));
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ models/blog.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const blogSchema = new mongoose.Schema({
+  title: { type: String, unique: true },
+  author: { type: String },
+  url: { type: String, unique: true },
+  likes: { type: Number },
+});
+
+blogSchema.plugin(uniqueValidator);
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
