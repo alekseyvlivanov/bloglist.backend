564afc97249b50b35784398c17e74c078e9dfca0
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-10-18 20:20:10 +1000

    feat: add task 4.4: helper functions and unit tests, step2 solution

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,11 @@
-// eslint-disable-next-line no-unused-vars
-const dummy = (blogs) => {
+const dummy = () => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => sum + item.likes;
+
+  return blogs.reduce(reducer, 0);
+};
+
+module.exports = { dummy, totalLikes };
