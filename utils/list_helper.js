de677f277a20bb50110d32f0a5c50a320fd170dd
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-10-18 22:56:37 +1000

    feat: add task 4.5*: helper functions and unit tests, step3 solution

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,10 +2,21 @@ const dummy = () => {
   return 1;
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+
+  const reducer = (fav, item) => (item.likes > fav.likes ? item : fav);
+  const { title, author, likes } = blogs.reduce(reducer);
+
+  return { title, author, likes };
+};
+
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => sum + item.likes;
 
   return blogs.reduce(reducer, 0);
 };
 
-module.exports = { dummy, totalLikes };
+module.exports = { dummy, favoriteBlog, totalLikes };
