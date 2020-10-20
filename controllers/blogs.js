a15a49e5f7b7038cdafe7edb1340283ad25bbce4
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-10-21 09:12:15 +1000

    feat: add task 4.19: bloglist expansion, step7 solution

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,16 @@
-/* eslint-disable no-underscore-dangle */
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 
 const Blog = require('../models/blog.js');
 const User = require('../models/user.js');
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -13,26 +21,31 @@ blogsRouter.post('/', async (request, response) => {
   const { body } = request;
 
   if (!body.title && !body.author) {
-    response.status(400).end();
-  } else {
-    const users = await User.find({});
-    const user = users[0];
-    const userJSON = user.toJSON();
-
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes || 0,
-      user: userJSON.id,
-    });
-
-    const savedBlog = await blog.save();
-    user.blogs = user.blogs.concat(savedBlog.id);
-    await user.save();
-
-    response.status(201).json(savedBlog);
+    return response.status(400).end();
   }
+
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).send({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user.id,
+  });
+
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog.id);
+  await user.save();
+
+  return response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
